dc3dd7b3e0caf3a5c0068f3572833b36fe81e4c6
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-01-17 19:16:11 +0400

    init: fix

--- src/index.js
+++ src/index.js
@@ -1,62 +1,30 @@
 module.exports = function toReadable (number) {
-let n = number;
-let newArr=[];
-let arr = n.toString().split('').map(a => Number(a));
 
-for (i=0;i<arr.length;i++){
+    let n = number;
+    
+        if (n===0){return 'zero'}
+    
+         const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+         const tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+         const tenner = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+        if(n<=9){ return ones[n] };
+    
+        if(n<=19){ return tens[n-10] };
+    
+        if(n<=99){ 
+          n = n.toString().split('');
+          return tenner[n[0]] + ' ' + ones[n[1]]
+        }
+    
+        if(n<=999){
+            n = n.toString().split('');
+        if(n[1]==='0') return ones[n[0]] + ' hundred ' + ones[n[2]];
+    
+        if(n[1]==='1') return ones[n[0]] + ' hundred ' + tens[n[2]];
+    
+        return ones[n[0]] + ' hundred ' + tenner[n[1]] + ' ' + ones[n[2]];
+        } 
+    
+        }
 
-
-let a = arr[i];
-
-    switch(a){
-        case 0:
-            newArr.push('');
-        break;
-
-        case 1:
-            newArr.push('one');
-        break;
-
-        case 2:
-            newArr.push('two');
-        break;
-
-        case 3:
-            newArr.push('three');
-        break;
-
-        case 4:
-            newArr.push('four');
-        break;
-
-        case 5:
-            newArr.push('five');
-        break;
-
-        case 6:
-            newArr.push('six');
-        break;
-
-        case 7:
-            newArr.push('seven');
-        break;
-
-        case 8:
-            newArr.push('eight');
-        break;
-
-        case 9:
-            newArr.push('nine');
-        break;
-
-
-        default: return;
-
-}
-}
-
-if (newArr.length > 2){
-    newArr.splice(1, 0, 'hundred')
-}
-return newArr.join(' ')
-}
